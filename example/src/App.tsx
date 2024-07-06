084904b40c976d154843017c5283494d20ba8d97
Alexander Revenko <revenko.a@bk.ru>
2024-07-06 16:04:08 +0700

    feat: реализовал useMediaQuery

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,21 +1,9 @@
-import { useState } from 'react';
-import useMediaQuery from '@lokixio/use-media-query';
+import HookExample from './HookExample';
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  const value = useMediaQuery();
-
   return (
     <>
-      <h1>{value}</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <HookExample />
     </>
   );
 }
--- use-media-query/src/index.ts
+++ use-media-query/src/index.ts
@@ -1,5 +1,29 @@
-function useMediaQuery() {
-  return 'hello from use-media-query';
+import { useEffect, useState } from 'react';
+
+interface HookArgs {
+  query: string;
 }
+
+type HandlerType = (ev: MediaQueryListEvent) => void;
+
+const useMediaQuery = ({ query }: HookArgs): boolean => {
+  const [matchValue, setMatchValue] = useState(false);
+
+  useEffect(() => {
+    const media = window.matchMedia(query);
+
+    if (media.matches !== matchValue) {
+      setMatchValue(media.matches);
+    }
+
+    const handler: HandlerType = ({ matches }) => setMatchValue(matches);
+
+    media.addEventListener('change', handler);
+
+    return () => media.removeEventListener('change', handler);
+  }, [matchValue, query]);
+
+  return matchValue;
+};
 
 export default useMediaQuery;
